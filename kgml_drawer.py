f508e19e200f5b23f59e034f24ef0397b85fc4db
Alexander Thomas Julian <ajulian@hawk.iit.edu>
2023-08-28 13:18:27 -0500

    Reworked custom accession handling

--- kgml_drawer.py
+++ kgml_drawer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 name = 'kgml_drawer.py'
-updated = '2023-08-12'
-version = '0.5.1'
+updated = '2023-08-28'
+version = '0.5.2'
 
 def kgml_drawer(custom_accessions=None,utk_dir=None,kegg_files_dir=None,kegg_locs_dir=None,map_aio=False,outdir="Filled_KEGG_Maps"):
 
@@ -71,28 +71,29 @@ def kgml_drawer(custom_accessions=None,utk_dir=None,kegg_files_dir=None,kegg_loc
 
 			line = line.strip()
 
-			if not samples:
-				
-				samples = line.split("\t")
+			if line != "":
 
-			else:
+				if line[0:2] == "##":
+					samples = line[2:].strip().split("\t")
 
-				data = line.split("\t")
+				elif line[0] != "#":
 
-				for index,ncbi in enumerate(data):
+					data = line.split("\t")
 
-					sample = samples[index]
+					for index,ncbi in enumerate(data):
 
-					if ncbi != "":
+						sample = samples[index]
 
-						if sample not in by_sample.keys():
-							by_sample[sample] = {}
+						if ncbi != "":
 
-						ko = kos[ncbi]
-						ec = ecs[ncbi]
+							if sample not in by_sample.keys():
+								by_sample[sample] = {}
 
-						by_sample[sample][ko] = True
-						by_sample[sample][ec] = True
+							ko = kos[ncbi]
+							ec = ecs[ncbi]
+
+							by_sample[sample][ko] = True
+							by_sample[sample][ec] = True
 
 		CUS.close()
 
@@ -175,30 +176,30 @@ def kgml_drawer(custom_accessions=None,utk_dir=None,kegg_files_dir=None,kegg_loc
 					(255,255,000,100), ## CYAN
 				]
 
-			## Create a legend
-			labels = [x for x in sorted(by_sample.keys())]
-			sizes = [len(x) for x in labels]
-			longest_label = labels[sizes.index(max(sizes))]
-			font = ImageFont.truetype("LiberationMono-Regular.ttf", size=1600)
-			text_width,text_height = font.getsize(longest_label)
+			# ## Create a legend
+			# labels = [x for x in sorted(by_sample.keys())]
+			# sizes = [len(x) for x in labels]
+			# longest_label = labels[sizes.index(max(sizes))]
+			# font = ImageFont.truetype("LiberationMono-Regular.ttf", size=1600)
+			# text_width,text_height = font.getsize(longest_label)
 
-			box_height = 17*100
-			box_width = 46*100
+			# box_height = 17*100
+			# box_width = 46*100
 
-			height = int(box_height*len(by_sample.keys())*1.5)
-			width = int(box_width*1.5) + int(text_width)
-			legend = Image.new('RGBA',(width,height),(255,255,255,0))
-			draw = ImageDraw.Draw(legend)
+			# height = int(box_height*len(by_sample.keys())*1.5)
+			# width = int(box_width*1.5) + int(text_width)
+			# legend = Image.new('RGBA',(width,height),(255,255,255,0))
+			# draw = ImageDraw.Draw(legend)
 
-			x1 = int(box_height*0.125)
-			y1 = int(box_width*0.125)
-			for index,sample in enumerate(by_sample.keys()):
-				draw.rectangle([x1,y1,x1+box_width,y1+box_height],colors[index])
-				draw.text((x1+int(box_width*1.25),y1),labels[index],(0,0,0),font=font)
-				y1 += int(box_height*1.5)
+			# x1 = int(box_height*0.125)
+			# y1 = int(box_width*0.125)
+			# for index,sample in enumerate(by_sample.keys()):
+			# 	draw.rectangle([x1,y1,x1+box_width,y1+box_height],colors[index])
+			# 	draw.text((x1+int(box_width*1.25),y1),labels[index],(0,0,0),font=font)
+			# 	y1 += int(box_height*1.5)
 
 
-			legend.save(f"{outdir}/AIO/legend.png")
+			# legend.save(f"{outdir}/AIO/legend.png")
 
 
 			## Open the KEGG map, create a blank canvas to draw over
@@ -249,7 +250,7 @@ def kgml_drawer(custom_accessions=None,utk_dir=None,kegg_files_dir=None,kegg_loc
 
 			out.save(f"{outdir}/AIO/ko{path}.png")
 
-		
+
 		## Sample by Sample
 		for sam in by_sample.keys():
 
