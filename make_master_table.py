5971da2c36e3c81b6b4681953c6024dc5ff47838
Alexander Thomas Julian <ajulian@hawk.iit.edu>
2023-08-27 21:19:31 -0500

    Add pathway to table; added single sample tables

--- make_master_table.py
+++ make_master_table.py
@@ -4,13 +4,17 @@ name = 'make_master_table.py'
 updated = '2023-08-12'
 version = '0.2.0'
 
-def make_master_table(prot_count_dir=None,ref_faa=None,conversion_file=None):
+def make_master_table(prot_count_dir=None,ref_faa=None,conversion_file=None,kegg_dir=None,outdir=None):
 	
-	from os import listdir
+	from os import listdir, makedirs
 	from os.path import isdir
+
+	if not isdir(outdir):
+		makedirs(outdir,mode=0o755)
 
 	annotations = {}
 	table_data = {}
+	accession_data = {}
 	ANNOT = open(ref_faa,'r')
 	for line in ANNOT:
 		line = line.strip()
@@ -20,6 +24,7 @@ def make_master_table(prot_count_dir=None,ref_faa=None,conversion_file=None):
 				annotation = " ".join(line.split(" ")[1:])
 				annotations[accession] = annotation
 				table_data[accession] = {'annot':annotation}
+				accession_data[accession] = []
 	ANNOT.close()
 
 
@@ -33,13 +38,21 @@ def make_master_table(prot_count_dir=None,ref_faa=None,conversion_file=None):
 		table_data[accession]['ecn'] = ecn
 	CONV.close()
 
+
+	path_by_ortho = {}
+	ORTH = open(f"{kegg_dir}/etc/ortho_pathways.tsv",'r')
+	for line in ORTH:
+		ortho,pathways = line.strip().split("\t")
+		path_by_ortho[ortho] = pathways
+	ORTH.close()
+
 	headers = False
-	samples = {}
+	sample_data = {}
 	for sample in listdir(prot_count_dir):
 
 		if sample != "temp_dir" and isdir(f"{prot_count_dir}/{sample}"):
 
-			samples[sample]= True
+			sample_data[sample] = []
 
 			COUNT = open(f"{prot_count_dir}/{sample}/count.tsv",'r')
 
@@ -56,44 +69,140 @@ def make_master_table(prot_count_dir=None,ref_faa=None,conversion_file=None):
 
 						table_data[accession]['len'] = seq_len
 						for i,x in enumerate(data[2:]):
+							if i == 0:
+								if int(x) > 0:
+									accession_data[accession].append(sample)
+									sample_data[sample].append(accession)
 							if headers[i] not in table_data[accession].keys():
 								table_data[accession][headers[i]] = {}
 							table_data[accession][headers[i]][sample] = x
 
 			COUNT.close()
 
+	exclusivity_data = {}
+	sample_ids = [x for x in sorted(sample_data.keys())]
+	for accession in accession_data.keys():
+		exc_string = ""
+		for sam in sample_ids:
+			if sam in accession_data[accession]:
+				exc_string = '1' + exc_string
+			else:
+				exc_string = '0' + exc_string
+		if exc_string not in exclusivity_data.keys():
+			exclusivity_data[exc_string] = []
+		exclusivity_data[exc_string].append(accession)
 
-	OUT = open(f"{prot_count_dir}/master_table.tsv",'w')
-
+	exc_ids = [x for x in sorted(exclusivity_data.keys())]
+	exc_dataset	= [exclusivity_data[x] for x in exc_ids]
+	max_iter = max([len(x) for x in exc_dataset])
 	
-	header_1 = ("\t"*len(samples.keys())).join(headers)
-	header_2 = "\t".join(("\t".join(sorted(samples.keys()))) for x in range(len(headers)))
-	OUT.write(f"## Accession\tProvided_UniProt_Accessions\tKEGG_ID\tKEGG_Ortholog\tEnzyme_Classification_Number\tAnnotation\tSequence_Length\t{header_1}\n")
-	OUT.write(f"##\t\t\t\t\t\t\t{header_2}\n")
-
-	for accession in table_data.keys():
-
-		OUT.write(accession)
-
-		upas = table_data[accession]['upas']
-		slen = table_data[accession]['len']
-		annot = table_data[accession]['annot']
-		kid = table_data[accession]['kid']
-		ko = table_data[accession]['ko']
-		ecn = table_data[accession]['ecn']
-
-		OUT.write(f"\t{upas}\t{kid}\t{ko}\t{ecn}\t{annot}\t{slen}")
-
-		for head in headers:
-
-			for sample in sorted(table_data[accession][head].keys()):
-
-				OUT.write(f"\t{table_data[accession][head][sample]}")
-
-		OUT.write("\n")
-
+	OUT = open(f"{outdir}/ExclusivityGroups.tsv",'w')
+	title1 = "\t".join(exc_ids)
+	OUT.write(f"## {title1}\n")
+	title2 = [":".join([sample_ids[len(sample_ids)-index-1] for index,char in enumerate(exc) if char == '1']) for exc in exc_ids]
+	title2[0] = 'MissingInAllSamples'
+	title2 = "\t".join(title2)
+	OUT.write(f"# {title2}\n")
+	for index in range(max_iter):
+		line = []
+		for dataset in exc_dataset:
+			if index < len(dataset):
+				line.append(dataset[index])
+			else:
+				line.append("")
+		line = "\t".join(line)
+		OUT.write(f"{line}\n")
 	OUT.close()
 
+	
+	def write_master(outpath,accessions_list,sample):
+
+		OUT = open(outpath,'w')
+		
+		if sample == 'all':
+			header_1 = ("\t"*len(sample_ids)).join(headers)
+			header_2 = "\t".join(("\t".join(sample_ids)) for x in range(len(headers)))
+			OUT.write(f"## Accession\tProvided_UniProt_Accessions\tEnzyme_Classification_Number\tKEGG_ID\tKEGG_Ortholog\tPathways\tAnnotation\tSequence_Length\t{header_1}\n")
+			OUT.write(f"#\t\t\t\t\t\t\t\t{header_2}\n")
+		else:
+			header_1 = ("\t").join(headers)
+			OUT.write(f"## Accession\tProvided_UniProt_Accessions\tEnzyme_Classification_Number\tKEGG_ID\tKEGG_Ortholog\tPathways\tAnnotation\tSequence_Length\t{header_1}\n")
+
+		for accession in accessions_list:
+
+			OUT.write(accession)
+
+			upas = table_data[accession]['upas']
+			slen = table_data[accession]['len']
+			annot = table_data[accession]['annot']
+			kid = table_data[accession]['kid']
+			ko = table_data[accession]['ko']
+			ecn = table_data[accession]['ecn']
+			
+			pathways = ""
+			if ko in path_by_ortho.keys():
+				pathways = path_by_ortho[ko]
+			else:
+				pathways = "N/A"
+
+			OUT.write(f"\t{upas}\t{ecn}\t{kid}\t{ko}\t{pathways}\t{annot}\t{slen}")
+
+			for head in headers:
+
+				if sample == 'all':
+
+					for sample in sample_ids:
+
+						OUT.write(f"\t{table_data[accession][head][sample]}")
+
+				else:
+
+					OUT.write(f"\t{table_data[accession][head][sample]}")
+
+			OUT.write("\n")
+
+		OUT.close()
+
+	write_master(f"{outdir}/master_table.tsv",sorted(table_data.keys()),'all')
+
+	temp_dir = f"{outdir}/sample_tables"
+	if not isdir(temp_dir):
+		makedirs(temp_dir,mode=0o755)
+	for sam in sample_ids:
+		write_master(f"{temp_dir}/{sam}_master_table.tsv",sample_data[sam],sam)
+
+
+	# temp_dir = f"{outdir}/exclusivity_tables"
+	# if not isdir(temp_dir):
+	# 	makedirs(temp_dir,mode=0o755)
+	
+	OUT = open(f"{outdir}/simple_exclusivity_table.tsv",'w')
+	title1 = "\t\t\t".join(exc_ids)
+	OUT.write(f"### {title1}\n")
+	title2 = [":".join([sample_ids[len(sample_ids)-index-1] for index,char in enumerate(exc) if char == '1']) for exc in exc_ids]
+	title2[0] = 'MissingInAllSamples'
+	title2 = "\t\t\t".join(title2)
+	OUT.write(f"## {title2}\n")
+	title3 = "\t".join([f"NCBI Accession\tAnnotation\tPathways" for x in range(len(exc_ids))])
+	OUT.write(f"# {title3}\n")
+	for index in range(max_iter):
+		line = []
+		for dataset in exc_dataset:
+			if index < len(dataset):
+				accession = dataset[index]
+				line.append(accession)
+				annot = table_data[accession]['annot']
+				line.append(annot)
+				ko = table_data[accession]['ko']
+				if ko in path_by_ortho.keys():
+					line.append(path_by_ortho[ko])
+				else:
+					line.append("N/A")
+			else:
+				line.append("")
+		line = "\t".join(line)
+		OUT.write(f"{line}\n")
+	OUT.close()
 
 if __name__ == "__main__":
 
@@ -104,11 +213,15 @@ if __name__ == "__main__":
 	GetOptions.add_argument("-p","--prot_count_dir",required=True)
 	GetOptions.add_argument("-r","--ref_faa",required=True)
 	GetOptions.add_argument("-c","--conv_file",required=True)
+	GetOptions.add_argument("-k","--kegg_dir",required=True)
+	GetOptions.add_argument("-o","--outdir",default="SummarizedData")
 
 	args = GetOptions.parse_known_args()[0]
 	prot_count_dir = args.prot_count_dir
 	conv_file = args.conv_file
 	ref_faa = args.ref_faa
+	kegg_dir = args.kegg_dir
+	outdir = args.outdir
 
 
-	make_master_table(prot_count_dir=prot_count_dir,ref_faa=ref_faa,conversion_file=conv_file)
+	make_master_table(prot_count_dir=prot_count_dir,ref_faa=ref_faa,conversion_file=conv_file,kegg_dir=kegg_dir,outdir=outdir)
